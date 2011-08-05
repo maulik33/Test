e2028eb0a79408f8c3d672f22d6c0901ebf4c3cc
Gokul Jayapal <gokul.jayapal@kaplan.com>
2011-08-05 15:10:24 -0500

    Added EmailRecipient.cs to Emailer project.
    [git-p4: depot-paths = "//Vendors/Act360/Utilities/RnEmailService/": change = 112459]

---
+++ Emailer/Entity/EmailRecipient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Emailer.Entity
+{
+    public class EmailRecipient
+    {
+        public string EmailId { get; set; }
+
+        public EmailSelectionLevel Type { get; set; }
+
+        public string Name { get; set; }
+    }
+}
